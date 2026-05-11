01dd0e1203cbaa0a02751728ef39c6903237f70f
Kan-Sangmuang <kansangmuang@gmail.com>
2026-05-11 14:37:20 -0500

    Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,13 @@
 
+
+
 class HelloWorld {
-    
+
+    // this is the standard signature for main in java
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+
+
+
 }
